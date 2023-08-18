783d43ffa508357d0afe9b539b65f16dadef636a
Ralph Tiri <ralpht08@gmail.com>
2023-08-17 21:49:26 -0400

    component update

--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -1,7 +1,48 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const CartValue = () => {
+    const { expenses, Location, budget, dispatch } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.unitprice);
+    }, 0);
+
+    const [editedBudget, setEditedBudget] = useState(budget);
+
+    const handleBudgetChange = (event) => {
+        setEditedBudget(event.target.value);
+    };
+
+    const handleBudgetUpdate = () => {
+        dispatch({
+            type: 'UPDATE_BUDGET',
+            payload: parseFloat(editedBudget),
+        });
+    };
+
+    return (
+        <div className="d-flex">
+        <div className='alert alert-primary mr-2'>
+            <span>Budget: </span> <input
+                        required='required'
+                        type='number'
+                        value={editedBudget}
+                        onChange={handleBudgetChange}
+                        step={10}
+                        >
+                            
+                       
+                        </input>          
+        </div>
+        <div className='alert alert-success mr-2' style={{margin: '10px'}}>
+            <span>Remaining: {Location}{editedBudget - totalExpenses}</span>
+        </div>
+        <div className='alert alert-info mr-2' style={{margin: '10px'}}>
+            <span>Spent so far: {Location}{totalExpenses}</span>
+        </div>
+
+        </div>
+    );
 };
 
 export default CartValue;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,31 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
-import { FaTimesCircle } from 'react-icons/fa';
+import { FaTimesCircle, FaPlusCircle, FaMinusCircle } from 'react-icons/fa';
 
 const ExpenseItem = (props) => {
+    const { dispatch, Location} = useContext(AppContext);
+
+    const handleDeleteItem = () => {
+        const item = {
+            name: props.name,
+        };
+
+        dispatch({
+            type: 'DELETE_ITEM',
+            payload: item,
+        });
+    };
+
+
+    return (
+        <tr>
+        <td>{props.name}</td>
+        <td>{Location}{parseInt(props.unitprice)}</td>
+        <td><FaPlusCircle size='2.2em' color="green" onClick={props.unitprice + 10}></FaPlusCircle></td>
+        <td><FaMinusCircle size='2.2em' color="red" onClick={props.unitprice + 10}></FaMinusCircle></td>
+        <td><FaTimesCircle size='1.2em' color="black" onClick={handleDeleteItem}></FaTimesCircle></td>
+        </tr>
+    );
 };
 
 export default ExpenseItem;
